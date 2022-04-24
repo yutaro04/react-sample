1062084402c8ab85be5a0a756c975e3757700619
yutaro-infratop <matsuo.yutaro.infratop@gmail.com>
2022-04-24 15:35:36 +0900

    change lang ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_jyKQCyPhq',
+    userPoolWebClientId: 'jj40to3l8qq61e1sknsutjt0h',
+    oauth: {
+      domain: 'testaro04410.auth.us-east-1.amazoncognito.com',
+      scope: ['openid'],
+      redirectSignIn: 'https://localhost:3000/',
+      redirectSignOut: 'https://localhost:3000/',
+      responseType: 'code'
+    }
+  }
+})
+
+const Example = () => {
+  const [user, setUser] = useState<any | null>(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const userData = await Auth.currentAuthenticatedUser();
+      // デバッグ用
+      Auth.currentSession().then((data) => {
+        console.log(`token: ${data.getIdToken().getJwtToken()}`);
+      });
+      console.log(userData);
+      return userData;
+    } catch (e) {
+      return console.log('Not signed in');
+    }
+  }
+
+  return user ? (
+    <div>
+      <p>サインイン済み</p>
+      <p>ユーザー名: {user.username}</p>
+      <button onClick={() => Auth.signOut()}>Sign Out</button>
+    </div>
+  ) : (
+    <div>
+      <p>
+        サインインする
+      </p>
+      <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
     </div>
   );
 }
 
-export default App;
+export default Example
